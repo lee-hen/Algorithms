c9c47149b3b3da9f4053d098aff16db0ca075312
lee-hen <leehenry303@gmail.com>
2021-08-24 20:23:18 +0900

    update..

--- 4_graphs/11_symbol_graph/main/main.go
+++ 4_graphs/11_symbol_graph/main/main.go
@@ -1,9 +1,8 @@
 package main
 
 import (
-	symbolGraph "github.com/lee-hen/Algorithms/4_graphs/11_symbol_graph"
-
 	"fmt"
+	symbolGraph "github.com/lee-hen/Algorithms/4_graphs/11_symbol_graph"
 	"os"
 )
 
@@ -12,5 +11,14 @@ func main()  {
 	f, _  := os.Open(pwd + "/data/routes.txt")
 	defer f.Close()
 	sg := symbolGraph.New(f, " ")
-	fmt.Println(sg.Graph())
+	g := sg.Graph()
+	fmt.Print(g)
+	fmt.Println("----------------------------")
+	for v := 0; v < g.V; v++ {
+		fmt.Print(sg.NameOf(v), ": ")
+		for _, w := range g.Adj(v) {
+			fmt.Print(sg.NameOf(w), " ")
+		}
+		fmt.Println()
+	}
 }

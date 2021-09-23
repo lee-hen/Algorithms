e26ee30b6d4b45c2ce00710b25508bf3c261c7fd
lee-hen <leehenry303@gmail.com>
2021-09-23 18:24:29 +0900

    Add solution of dining philosophers problems

--- 5_context_or_beyond/101_go_routines/18_dining_philosophers/main.go
+++ 5_context_or_beyond/101_go_routines/18_dining_philosophers/main.go
@@ -5,24 +5,30 @@ import (
 	"sync"
 )
 
+var host = make(chan bool, 2)
+var wg sync.WaitGroup
+
 type Chopstick struct{ sync.Mutex }
 
 type Philosopher struct {
+	idx int
 	leftCS, rightCS *Chopstick
 }
 
-var wg sync.WaitGroup
-
 // All philosophers might lock their left chopsticks concurrently
 // All chopsticks would be locked
-// None can lock their right chopsticks
+// No one can lock their right chopsticks
 func (p Philosopher) eat() {
 	for {
+		host <- true
+
 		p.leftCS.Lock()
 		p.rightCS.Lock()
-		fmt.Println("eating...")
+		fmt.Println(p.idx, "is eating...")
 		p.rightCS.Unlock()
 		p.leftCS.Unlock()
+
+		<-host
 	}
 	wg.Done()
 }
@@ -35,11 +41,11 @@ func main() {
 
 	philosophers := make([]*Philosopher, 5)
 	for i := 0; i < 5; i++ {
-		philosophers[i] = &Philosopher{chopsticks[i], chopsticks[(i+1)%5]}
+		philosophers[i] = &Philosopher{i,chopsticks[i], chopsticks[(i+1)%5]}
 	}
 
-	wg.Add(5)
 	for i := 0; i < 5; i++ {
+		wg.Add(1)
 		go philosophers[i].eat()
 	}
 	wg.Wait()
